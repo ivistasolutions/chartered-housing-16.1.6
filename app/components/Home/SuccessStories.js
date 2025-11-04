8e97f63561d2e0677ae9ed0627974ede8f772b83
Bhavankumarg <gbhavanbkr@gmail.com>
2025-11-04 17:34:03 +0530

    youtube video on home page

--- app/components/Home/SuccessStories.js
+++ app/components/Home/SuccessStories.js
@@ -12,8 +12,22 @@ const SuccessStories = () => {
           <h2 className="text-4xl pt-1 md:text-6xl font-bold text-[#646464] roboto-serif-regular">
             Success Stories
           </h2>
-          {/* <div className="lg:w-56 w-32 border-t-2 border-[#ED1C24] my-2"></div> */}
           <div className="lg:w-44 w-32 border-b-2 border-[#ED1C24] mt-3 mb-6 mx-1"></div>
+          <div className="w-full pt-10">
+            <div className="relative w-full pb-[56.25%] h-0 overflow-hidden">
+              <iframe
+                className="absolute top-0 left-0 w-full h-full"
+                src="https://www.youtube.com/embed/A7aS5pIxSFM?autoplay=1&mute=1&controls=1&modestbranding=1&rel=0&iv_load_policy=3&cc_load_policy=0&playsinline=1"
+                title="At Home with Chartered Housing | Mr. Prabhu &amp; Family on Life at Chartered Grasshopper"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
+            </div>
+            {/* <div className="lg:w-56 w-32 border-t-2 border-[#ED1C24] my-2"></div> */}
+            <div className="lg:w-44 w-32 border-b-2 border-[#ED1C24] mt-3 mb-6 mx-1"></div>
+          </div>
         </div>
         <div>
           <TestimonialsSection />
